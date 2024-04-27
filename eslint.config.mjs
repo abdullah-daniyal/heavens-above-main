c3eb7745c0cb0b7bb1909e1452dc0ddcb14fd046
Ab <Harry@codewithAb.com>
2024-04-27 11:00:49 +0500

    updated eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,15 @@
 export default {
-  files: ["**/*.js"],
-  languageOptions: {
-    ecmaVersion: 2021,
-    sourceType: "module",
-    globals: {
-      $: "readonly",
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+      globals: {
+        $: "readonly",
+      },
     },
-  },
-  rules: {
-    "no-unused-vars": "warn",
-    "no-console": "off",
-  },
-};
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off",
+    },
+  };
+  
